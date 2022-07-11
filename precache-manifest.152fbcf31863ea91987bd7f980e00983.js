a1a2eea2b6e6b71bf6ddd5903db89f6c0c7efa97
Rishav Gupta <77620644+iamrishavgupta@users.noreply.github.com>
2022-07-11 14:15:26 +0530

    Updates

---
+++ precache-manifest.152fbcf31863ea91987bd7f980e00983.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b612816f3292d3b3565780d00f0f1c",
+    "url": "/spotify-clone/index.html"
+  },
+  {
+    "revision": "9e05878a7fed4b1a0462",
+    "url": "/spotify-clone/static/css/main.fb7fe8de.chunk.css"
+  },
+  {
+    "revision": "b75143f5c43e3b17cadf",
+    "url": "/spotify-clone/static/js/2.96260b78.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/spotify-clone/static/js/2.96260b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e05878a7fed4b1a0462",
+    "url": "/spotify-clone/static/js/main.36e0ec63.chunk.js"
+  },
+  {
+    "revision": "52de21c6b549d6d1baa2",
+    "url": "/spotify-clone/static/js/runtime-main.f1339594.js"
+  }
+]);
